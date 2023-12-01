9d890b173f662bdaab40dbe2050c8c2f84bdf7ff
Qasim-Bukhari <qasim.secondry@gmail.com>
2023-12-01 13:16:06 +0500

    added index.html

---
+++ app.js
@@ -0,0 +1,489 @@
+// --- CHAPTER#5 ---
+
+// --- CHAPTER#5 ---:1
+
+// var number_1 = 8;
+// var number_2 = 4;
+// var result = number_1 + number_2;
+// document.write("Sum of " + number_1 + " and " + number_2 + " is " + result + "<br>");
+
+// --- CHAPTER#5 ---:2
+
+// var number_1 = 8;
+// var number_2 = 4;
+// var result = number_1 - number_2;
+// document.write("Subtraction of " + number_1 + " and " + number_2 + " is " + result + "<br>");
+
+// var number_1 = 8;
+// var number_2 = 4;
+// var result = number_1 * number_2;
+// document.write("Multiplication of " + number_1 + " and " + number_2 + " is " + result + "<br>");
+
+// var number_1 = 8;
+// var number_2 = 4;
+// var result = number_1 / number_2;
+// document.write("Division of " + number_1 + " and " + number_2 + " is " + result + "<br>");
+
+// var number_1 = 8;
+// var number_2 = 4;
+// var result = number_1 % number_2;
+// document.write("Modulus of " + number_1 + " and " + number_2 + " is " + result + "<br>");
+
+// --- CHAPTER#5 ---:3
+
+// var number;
+// document.write("Value after variable declaration is " + number + "<br>");
+
+// number = 5;
+// document.write("Initial value is " + number + "<br>");
+
+// number++;
+// document.write("Value after increment is " + number + "<br>");
+
+// number = number + 7;
+// document.write("Value after addition is " + number + "<br>");
+
+// number--;
+// document.write("Value after decrement is " + number + "<br>");
+
+// number = number % 3;
+// document.write("Output is " + number + "<br>");
+
+// --- CHAPTER#5 ---:4
+
+// var ticketCost = 600;
+// var totalTickets = 5;
+// var totalCost = ticketCost * totalTickets;
+// document.write("The cost to buy " + totalTickets + " ticket(s) to a movie is " + totalCost + " Rs." + "<br>");
+
+// --- CHAPTER#5 ---:5
+
+// var number = 4;
+// var timesMultiplied = 1;
+// var output = number * timesMultiplied;
+// document.write("Table of " + number + "<br>");
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// --- CHAPTER#5 ---:6
+
+// var celciusTemperature = 45;
+// var celciusToFahrenheit = celciusTemperature * (9/5) + 32
+// document.write(celciusTemperature + " is " + celciusToFahrenheit);
+
+// var fahrenheitTemperature = 113;
+// var fahrenheitToCelcius =  (fahrenheitTemperature - 32) * 5/9;
+// document.write(fahrenheitTemperature + " is " + fahrenheitToCelcius);
+
+// --- CHAPTER#5 ---:7
+
+// var productOnePrice = 650;
+// var productTwoPrice = 100;
+// var productOneQuantity = 3;
+// var productTwoQuantity = 7;
+// var shippingCharges = 100;
+
+// var productOneTotal = productOnePrice * productOneQuantity;
+// var productTwoTotal = productTwoPrice * productTwoQuantity;
+// var total = productOneTotal + productTwoTotal + shippingCharges;
+
+// document.write("Price of item 1 is " + productOnePrice + "<br>");
+// document.write("Quantity of item 1 is " + productOneQuantity + "<br>");
+// document.write("Price of item 2 is " + productTwoPrice + "<br>");
+// document.write("Quantity of item 2 is " + productTwoQuantity + "<br>");
+// document.write("Shipping charges " + shippingCharges + "<br>");
+// document.write("Total cost of your order is " + total + "<br>");
+
+// --- CHAPTER#5 ---:8
+
+// var totalMarks = 980;
+// var obtainedMarks = 804;
+// var percentage = (obtainedMarks / totalMarks) * 100;
+
+// document.write("Total marks are " + totalMarks + "<br>");
+// document.write("Obtained marks are " + obtainedMarks + "<br>");
+// document.write("Percentage is " + percentage + "<br>");
+
+// --- CHAPTER#5 ---:9
+
+// var usdToPKR = 275;
+// var SARToPKR = 73.31;
+// var totalUSD = 10;
+// var totalSAR = 25;
+// var totalCurrencyInPKR = ((totalUSD * usdToPKR) + (totalSAR * SARToPKR));
+
+// document.write("Total currency in PKR is " + totalCurrencyInPKR);
+
+// --- CHAPTER#5 ---:10
+
+// var number = 5;
+// var output = (((number + 5) * 10) / 2);
+// document.write("The output is " + output + "<br>");
+
+// --- CHAPTER#5 ---:11
+
+// var currentYear = 2023;
+// var birthYear = 2003;
+// var expectedAge = currentYear - birthYear;
+
+// document.write("Current year is " + currentYear + "<br>");
+// document.write("Birth year is " + birthYear + "<br>");
+// document.write("Your expected Age is " + expectedAge + "<br>");
+
+// expectedAge--;
+
+// document.write("If your birth date is yet to come this year than your age is " + expectedAge);
+
+// --- CHAPTER#5 ---:12
+
+// var radius = 5;
+// var circumference = 2 * 3.142 * radius;
+// var area = 3.142 * (radius * radius);
+
+// document.write("Radius of a circle is " + radius + "<br>");
+// document.write("Circumference of a circle is " + circumference + "<br>");
+// document.write("Area of a circle is " + area + "<br>");
+
+// --- CHAPTER#5 ---:13
+
+// var favouriteSnack = "Chips";
+// var currentAge = 20;
+// var estimatedAge = 60;
+// var perDayCons_ = 2;
+// var perYearCons_ = perDayCons_ * 365;
+// var snacksNeeded = ((estimatedAge - currentAge) * perYearCons_);
+
+// document.write("You need " + snacksNeeded + " " + favouriteSnack + " to last, until your age will be " + estimatedAge + "<br>");
+
+// ---CHAPTER#6-9---
+
+// ---CHAPTER#6-9---:1
+
+// var a = 10;
+
+// document.write(`The value of a is: ${a}` + "<br>");
+
+// document.write(`The value of ++a is: ${++a}` + "<br>");
+// document.write(`Now the value of a is: ${a}` + "<br>");
+
+// document.write(`The value of a++ is: ${a++}` + "<br>");
+// document.write(`Now the value of a is: ${a}` + "<br>");
+
+// document.write(`The value of --a is: ${--a}` + "<br>");
+// document.write(`Now the value of a is: ${a}` + "<br>");
+
+// document.write(`The value of a-- is: ${a--}` + "<br>");
+// document.write(`Now the value of a is: ${a}` + "<br>");
+
+// ---CHAPTER#6-9---:2
+
+// var a = 2;
+// var b = 1;
+// var result = --a;
+// var result = (--a) - (--b);
+// var result = (--a) - (--b) + (++b) + (b--);
+
+
+// document.write("The output of a is " + a + "<br>");
+// document.write("The output of b is " + b + "<br>");
+// document.write("The resul of --a is " + result +  "<br>");
+// document.write("The result of --a - --b is " + result +  "<br>");
+// document.write("The result of --a - --b + ++b + b-- is " + result +  "<br>");
+
+// ---CHAPTER#6-9---:3
+
+// var name = prompt("Enter your name");
+// alert("Welcome to our website " + name);
+
+// ---CHAPTER#6-9---:4
+
+// var number = +prompt("Enter any number", 5);
+// var timesMultiplied = 1;
+// var output = number * timesMultiplied;
+// document.write("Table of " + number + "<br>");
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// timesMultiplied++;
+// output = number * timesMultiplied;
+// document.write(number + " * " + timesMultiplied + " = " + output + "<br>");
+
+// ---CHAPTER#6-9---:5
+
+// var subjectOne = prompt("Enter subject one");
+// var subjectTwo = prompt("Enter subject two");
+// var subjectThree = prompt("Enter subject Three");
+
+// var totalMarksSubjectOne = 100;
+// var totalMarksSubjectTwo = 100;
+// var totalMarksSubjectThree = 100;
+
+// var obtainedMarksSubjectOne = +prompt("Enter obtained marks in subject one");
+// var obtainedMarksSubjectTwo = +prompt("Enter obtained marks in subject one");
+// var obtainedMarksSubjectThree = +prompt("Enter obtained marks in subject one");
+
+// var percentageSubjectOne = (obtainedMarksSubjectOne / totalMarksSubjectOne) * 100;
+// var percentageSubjectTwo = (obtainedMarksSubjectTwo / totalMarksSubjectTwo) * 100;
+// var percentageSubjectThree = (obtainedMarksSubjectThree / totalMarksSubjectThree) * 100;
+
+// var combinedTotalMarks = totalMarksSubjectOne + totalMarksSubjectTwo + totalMarksSubjectThree;
+// var combineObtainedMarks = obtainedMarksSubjectOne + obtainedMarksSubjectTwo + obtainedMarksSubjectThree;
+// var combinedTotalPercentage = (combineObtainedMarks / combinedTotalMarks) * 100;
+
+// document.write(subjectOne + ", " + subjectTwo + ", " + subjectThree + "<br>");
+// document.write(totalMarksSubjectOne + ", " + totalMarksSubjectTwo + ", "  + totalMarksSubjectThree + "<br>");
+// document.write(obtainedMarksSubjectOne + ", " + obtainedMarksSubjectTwo + ", "  + obtainedMarksSubjectThree + "<br>");
+// document.write(percentageSubjectOne + "%, " + percentageSubjectTwo + "%, "  + percentageSubjectThree + "%, " + "<br>")
+// document.write(combinedTotalMarks + ",  " + combineObtainedMarks + ", " + combinedTotalPercentage + "<br>");
+
+// ---CHAPTER#9-11---
+
+// ---CHAPTER#9-11---1
+
+// var cityName = prompt("Enter city name");
+
+// if(cityName === "karachi" || "Karachi") {
+//     alert("Welcome to the City of Lights");
+// }
+
+// ---CHAPTER#9-11---2
+
+// var gender = prompt("Enter your gender");
+
+// if(gender === "male" || "Male") {
+//     alert("Good Morning, Sir!");
+// }
+
+// else if(gender === "female" || "Female") {
+//     alert("Good Morning, Ma'am!");
+// }
+
+// else {
+//     alert("Good Morning");
+// }
+
+// ---CHAPTER#9-11---3
+
+// var trafficSignalColor = prompt("Enter traffic signal color");
+
+// if(trafficSignalColor === "red" || "Red") {
+//     alert("Must stop.");
+// }
+
+// else if(trafficSignalColor === "yellow" || "Yellow") {
+//     alert("Ready to move.");
+// }
+
+// else if(trafficSignalColor === "green" || "Green") {
+//     alert("Move now.");
+// }
+
+// else  {
+//     alert("Choose correct color, please.");
+// }
+
+// ---CHAPTER#9-11---4
+
+// var remainingFuel = prompt("Enter remaining fuel in litres");
+
+// if(remainingFuel < 0.25) {
+//     alert("Please, refill the fuel in your car.");
+// }
+
+// else {
+//     alert("You have enough fuel to drive.");
+// }
+
+// ---CHAPTER#9-11---5(a)
+
+// var a = 4;
+
+// if (++a === 5) {
+//     alert("Given condition for variable a is true");
+// }
+
+// else {
+//     alert("Given condition is wrong.");
+// }
+
+// // ---CHAPTER#9-11---5(b)
+
+// var b = 82;
+
+// if (b++ === 83) {
+//     alert("Given condition for variable b is true");
+// }
+
+// else {
+//     alert("Given condition is wrong.");
+// }
+
+// // ---CHAPTER#9-11---5(c)
+
+// var c = 12;
+
+// if (c++ === 13) {
+//     alert("condition 1 is true");
+// }
+
+// if (c === 13) {
+//     alert("condition 2 is true");
+// }
+
+// if (++c < 14) {
+//     alert("condition 3 is true");
+// }
+
+// if(c === 14) {
+//     alert("condition 4 is true");
+// }
+
+// else {
+//     alert("Given condition is wrong.");
+// }
+
+// ---CHAPTER#9-11---5(d)
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+
+// if (totalCost === laborCost + materialCost){
+//     alert("The cost equals");
+// }
+
+// else {
+//     alert("Given condition is wrong.");
+// }
+
+// ---CHAPTER#9-11---5(e)
+
+// var inputBoolean = prompt("Enter true or false value");
+
+// if(inputBoolean === "true" || "True") {
+//     alert("True");
+// }
+
+// else if(inputBoolean === "false" || "False") {
+//     alert("False");
+// }
+
+// else {
+//     alert("Given condition is wrong.");
+// }
+
+// ---CHAPTER#9-11---6
+
+// var totalMarksSubjectOne = 100;
+// var totalMarksSubjectTwo = 100;
+// var totalMarksSubjectThree = 100;
+
+// var obtainedMarksSubjectOne = +prompt("Enter obtained marks in subject one");
+// var obtainedMarksSubjectTwo = +prompt("Enter obtained marks in subject two");
+// var obtainedMarksSubjectThree = +prompt("Enter obtained marks in subject three");
+
+// var percentageSubjectOne = (obtainedMarksSubjectOne / totalMarksSubjectOne) * 100;
+// var percentageSubjectTwo = (obtainedMarksSubjectTwo / totalMarksSubjectTwo) * 100;
+// var percentageSubjectThree = (obtainedMarksSubjectThree / totalMarksSubjectThree) * 100;
+
+// var combinedTotalMarks = totalMarksSubjectOne + totalMarksSubjectTwo + totalMarksSubjectThree;
+// var combineObtainedMarks = obtainedMarksSubjectOne + obtainedMarksSubjectTwo + obtainedMarksSubjectThree;
+// var combinedTotalPercentage = (combineObtainedMarks / combinedTotalMarks) * 100;
+
+// if(combinedTotalPercentage >= 80) {
+//     document.write("<h1>Mark Sheet</h1>" + "<br>")
+//     document.write("Total Marks " + combinedTotalMarks + "<br>");
+//     document.write("Obtained Marks " + combineObtainedMarks + "<br>");
+//     document.write("Percentage " + combinedTotalPercentage + "<br>");
+//     document.write("Grade A-one" + "<br>");
+// }
+
+// else if(combinedTotalPercentage >= 70) {
+//     document.write("<h1>Mark Sheet</h1>" + "<br>")
+//     document.write("Total Marks " + combinedTotalMarks + "<br>");
+//     document.write("Obtained Marks " + combineObtainedMarks + "<br>");
+//     document.write("Percentage " + combinedTotalPercentage + "<br>");
+//     document.write("Grade A" + "<br>");
+// }
+
+// else if(combinedTotalPercentage >= 60) {
+//     document.write("<h1>Mark Sheet</h1>" + "<br>")
+//     document.write("Total Marks " + combinedTotalMarks + "<br>");
+//     document.write("Obtained Marks " + combineObtainedMarks + "<br>");
+//     document.write("Percentage " + combinedTotalPercentage + "<br>");
+//     document.write("Grade B" + "<br>");
+// }
+
+// else if(combinedTotalPercentage < 60) {
+//     document.write("<h1>Mark Sheet</h1>" + "<br>")
+//     document.write("Total Marks " + combinedTotalMarks + "<br>");
+//     document.write("Obtained Marks " + combineObtainedMarks + "<br>");
+//     document.write("Percentage " + combinedTotalPercentage + "<br>");
+//     document.write("Grade Fail" + "<br>");
+// }
